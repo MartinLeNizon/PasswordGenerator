6dabce92c378bbff248779a523aae70d1bb820fe
MartinLeNizon <nizonmartin@gmail.com>
2023-07-11 19:45:53 +0200

    Length argument added

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,13 @@
 import random
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--length', type=int, default=random.randint(8, 15), help='The number of character of the password')
+args = parser.parse_args()
 
 password = ""
-password_length = random.randint(8, 15)
+password_length = args.length
+
 
 for i in range(password_length):
 		password += chr(random.randint(33, 125))
